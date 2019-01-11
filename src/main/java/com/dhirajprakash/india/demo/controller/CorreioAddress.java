645070b83e96b9931736a87cfdb8c47a164526f1
Dhiraj Prakash <dhiraj@brazhuman.com>
2019-01-10 23:17:49 -0200

    Entity working,Remember to use java.persistence for Id

--- src/main/java/com/dhirajprakash/india/demo/controller/CorreioAddress.java
+++ src/main/java/com/dhirajprakash/india/demo/controller/CorreioAddress.java
@@ -1,10 +1,6 @@
 package com.dhirajprakash.india.demo.controller;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "CORREIO_ADDRESS")
@@ -13,11 +9,74 @@ public class CorreioAddress {
 	@Id
 	@Column(name = "ID")
 	String id;
+
+	@Column(name = "BAIRRO")
 	String bairro;
+	@Column(name = "CEP")
 	String cep;
+	@Column(name = "CIDADE")
 	String cidade;
+	@Column(name = "COMPLEMENTO")
 	String complemento2;
+	@Column(name = "END")
 	String end;
+	@Column(name = "UF")
 	String uf;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getComplemento2() {
+		return complemento2;
+	}
+
+	public void setComplemento2(String complemento2) {
+		this.complemento2 = complemento2;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
 }
