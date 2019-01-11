74631fe65a3f86a233df872bd42fb6d283053787
Dhiraj Prakash <dhiraj@brazhuman.com>
2019-01-10 22:43:21 -0200

    JUnit test called

--- src/test/java/com/dhirajprakash/india/demo/DemoApplicationTests.java
+++ src/test/java/com/dhirajprakash/india/demo/DemoApplicationTests.java
@@ -2,16 +2,28 @@ package com.dhirajprakash.india.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.dhirajprakash.india.demo.controller.DemoService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	DemoService demoService;
+
+	private Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+
 	@Test
 	public void contextLoads() {
+		logger.info("test called");
+		demoService.calculate();
+
 	}
 
 }
-
