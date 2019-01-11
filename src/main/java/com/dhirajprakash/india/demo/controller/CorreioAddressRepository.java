68b675aec8457a99eeef5948a0760dec1d1b609c
Dhiraj Prakash <dhiraj@brazhuman.com>
2019-01-10 23:22:16 -0200

    CrudRepository Added

---
+++ src/main/java/com/dhirajprakash/india/demo/controller/CorreioAddressRepository.java
@@ -0,0 +1,7 @@
+package com.dhirajprakash.india.demo.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CorreioAddressRepository extends CrudRepository<CorreioAddress, String> {
+
+}
