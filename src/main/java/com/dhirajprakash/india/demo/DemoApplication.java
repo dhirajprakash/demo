17eb61944e4c02334ccd26e533b9159a31619f5d
Dhiraj Prakash <dhiraj.prakash.30aug@gmail.com>
2019-01-14 15:00:04 -0200

    Firt commit of hibernate class

--- src/main/java/com/dhirajprakash/india/demo/DemoApplication.java
+++ src/main/java/com/dhirajprakash/india/demo/DemoApplication.java
@@ -7,18 +7,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@SpringBootApplication
-//public class DemoApplication extends SpringBootServletInitializer {
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(DemoApplication.class);
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-//	}
-//}
-
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
